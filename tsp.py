d402d93bb5209b5572281b4e514066bdbe890257
John Balat <jbalat87@gmail.com>
2017-06-09 01:07:16 +0000

    Added 2-opt algorithm

--- tsp.py
+++ tsp.py
@@ -37,6 +37,59 @@ def nearest_neighbor(cities):
     distance = find_distance(nearest, start)
     path_distance += distance
     return (path, path_distance)
+    
+    
+def get_total_distance(cities):
+    
+    total_distance = 0
+    last_city = None
+    
+    for i in xrange(len(cities) - 1):
+        current_city = cities[i]
+        next_city = cities[i + 1]
+        
+        total_distance += find_distance(current_city, next_city)
+        last_city = next_city
+    
+    #Add the distance to go back to the starting city
+    total_distance += find_distance(last_city, cities[0])
+    
+    return total_distance
+    
+    
+def two_opt_swap(route, i, j):
+    
+    new_route = route[0:i]
+    new_route.extend(reversed(route[i:j+1]))
+    new_route.extend(route[j+1:])
+    
+    return new_route
+    
+    
+def two_opt(cities):
+    isImproved = True
+    
+    best_route = cities
+    best_distance = get_total_distance(best_route)
+    
+    while isImproved:
+        isImproved = False
+        
+        for i in xrange(len(best_route) - 1):
+            for j in xrange(i + 1, len(best_route)):
+                new_route = two_opt_swap(best_route, i, j)
+                new_distance = get_total_distance(new_route)
+                
+                if new_distance < best_distance:
+                    best_distance = new_distance
+                    best_route = new_route
+                    isImproved = True
+                    break
+                
+            if isImproved:
+                break
+
+    return best_route, best_distance
 
 
 def write_results(filename, path, distance):
@@ -53,6 +106,7 @@ if __name__ == '__main__':
     start = timer()
     cities = read_file(input_file)
     (path, path_distance) = nearest_neighbor(cities)
+    # (path, path_distance) = two_opt(cities)
     write_results(output_file, path, path_distance)
     end = timer()
     print('Time: %s seconds' % str(end - start))
